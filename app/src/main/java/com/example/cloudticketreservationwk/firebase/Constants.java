8d21c69389e32fd13967d681afeb632069ede014
hibamai <hibataha2012@hotmail.com>
2026-03-02 23:10:17 -0500

    Revert "US #2.1 and US #2.4"
    This reverts commit 353ddbd9efa953985378e957c626c53d899b2b2d.

--- app/src/main/java/com/example/cloudticketreservationwk/firebase/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.example.cloudticketreservationwk.firebase;
-
-public class Constants {
-    public static final String USERS_COLLECTION = "users";
-    public static final String ROLE_CUSTOMER = "CUSTOMER";
-    public static final String ROLE_ADMIN = "ADMIN";
-}
